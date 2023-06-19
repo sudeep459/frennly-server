72657e0e10ebbf1472280e04aed55972fd6fa686
Sudeep Kumar Nemani <sudeep459@gmail.com>
2023-06-20 00:15:03 +0530

    Add template code for the backend applicaiton
    Created a new spring appliation for the server side code of frennly
    web application.

---
+++ src/test/java/com/frennly/ds/MainBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frennly.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
