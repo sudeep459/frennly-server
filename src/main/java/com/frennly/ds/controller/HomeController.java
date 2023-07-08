f4022a4c75773e9884dbaea0c2f178b266e5dad8
Sudeep Kumar Nemani <sudeep459@gmail.com>
2023-07-08 12:11:27 +0530

    add cron job for every 5 minutes

--- src/main/java/com/frennly/ds/controller/HomeController.java
+++ src/main/java/com/frennly/ds/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.frennly.ds.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
+@Slf4j
 public class HomeController {
 
     @GetMapping("/")
     public ResponseEntity<String> HomeController() {
         return ResponseEntity.status(HttpStatus.OK).body("welcome to frennly api");
+    }
+    @Scheduled(cron = "0 */5 * ? * *")
+    public void runEvey5Minutes() {
+        log.info("Current time is :: " + LocalDate.now());
     }
 }
