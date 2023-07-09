8f526f8e123be247871ac2766d4ce5b69d305a6a
Sudeep Kumar Nemani <sudeep459@gmail.com>
2023-07-09 13:10:13 +0530

    updated db conn to render db

--- src/main/java/com/frennly/ds/controller/RealTimeChat.java
+++ src/main/java/com/frennly/ds/controller/RealTimeChat.java
@@ -6,7 +6,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class RealTimeChat {
 
     @Autowired
@@ -15,6 +17,7 @@ public class RealTimeChat {
     @MessageMapping("/message")
     @SendTo("/group/public")
     public Message recieveMessage(@Payload Message message) {
+
         simpMessagingTemplate.convertAndSend("/group/" + message.getChat().getId().toString(), message);
         return message;
     }
