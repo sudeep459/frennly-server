efaab9424a9d8127f516246ce6e6a39bbf5a58f3
Dn0711 <dilip.nemani@spotflock.com>
2024-06-02 00:57:45 +0530

    Add all origin whitelist

--- src/main/java/com/frennly/ds/config/SecurityConfig.java
+++ src/main/java/com/frennly/ds/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                                 "http://13.55.80.60:3000/",
                                 "http://13.55.80.60/",
                                 "https://therapy-mauve.vercel.app/",
-                                "http://friennly.in/"
+                                "http://friennly.in/",
+                                "*"
                         ));
                         cfg.setAllowedMethods(Collections.singletonList("*"));
                         cfg.setAllowCredentials(true);
