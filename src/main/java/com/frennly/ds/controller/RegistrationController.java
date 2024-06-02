35975f84275614c57a18f816435bf575d00c1f9d
Dn0711 <dilip.nemani@spotflock.com>
2024-06-02 12:52:03 +0530

    Add all origin whitelist

--- src/main/java/com/frennly/ds/controller/RegistrationController.java
+++ src/main/java/com/frennly/ds/controller/RegistrationController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller
+@Controller("/api")
 public class RegistrationController {
 
     @Autowired
